3cedd4af0d66a961ce5c4207764408f3149b9804
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-09-06 12:38:15 +0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -38,7 +38,7 @@ provider "azurerm" {
   features {}
 }
 
-rovider "kubernetes" {
+provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
   client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
