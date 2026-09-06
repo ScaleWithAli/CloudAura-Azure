0470dcfc5ee6e2a386c00a309672b82fc08f1a8e
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-09-06 10:10:00 +0500

    Remove azurerm_role_assignment for Key Vault
    Removed role assignment for Key Vault Secrets Officer.

--- database.tf
+++ database.tf
@@ -75,11 +75,6 @@ resource "azurerm_key_vault" "main" {
   tags                      = local.common_tags
 }
 
-resource "azurerm_role_assignment" "kv_admin" {
-  scope                = azurerm_key_vault.main.id
-  role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
 
 resource "azurerm_key_vault_secret" "service_secrets" {
   for_each     = local.services
