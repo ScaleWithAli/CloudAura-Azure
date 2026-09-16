29991d44ce79a6f0e338c5f225bc8e6c33762594
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-09-16 08:37:57 +0500

    remove kubernetes namespace resource

--- terraform/identity.tf
+++ terraform/identity.tf
@@ -40,12 +40,6 @@ resource "azurerm_role_assignment" "external_secrets_keyvault" {
   scope                = azurerm_key_vault.main.id
 }
 
-resource "kubernetes_namespace" "external_secrets" {
-  metadata {
-    name = "external-secrets"
-  }
-}
-
 resource "kubernetes_service_account" "external_secrets_sa" {
   metadata {
     name      = "external-secrets-sa"
@@ -54,5 +48,4 @@ resource "kubernetes_service_account" "external_secrets_sa" {
       "azure.workload.identity/client-id" = azurerm_user_assigned_identity.external_secrets.client_id
     }
   }
-  depends_on = [kubernetes_namespace.external_secrets]
 }
